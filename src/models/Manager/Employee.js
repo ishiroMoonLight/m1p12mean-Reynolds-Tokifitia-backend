b16e2b628e425b35c094636684be684e0ae160d7
ishiroMoonLight <reynoroch@gmail.com>
2025-04-03 23:38:35 +0200

    Update Employee.js
    I removed the password since we aint gonna use it

--- src/models/Manager/Employee.js
+++ src/models/Manager/Employee.js
@@ -6,19 +6,19 @@ const employeeSchema = new mongoose.Schema({
   firstname: { type: String, required: true },
   bio: { type: String },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: false },
   pfp: { type: String },  // Link to profile picture
   salary: { type: Number, required: true },
   createdAt: { type: Date, default: Date.now },
   reparation: { type: mongoose.Schema.Types.Mixed, ref: "Reparation", default: null }
 }, { timestamps: true });
 
-employeeSchema.pre('save', async function (next) {
-  if (this.isModified('password')) {
-    this.password = await bcrypt.hash(this.password, 10);
-  }
-  next();
-});
+// employeeSchema.pre('save', async function (next) {
+//   if (this.isModified('password')) {
+//     this.password = await bcrypt.hash(this.password, 10);
+//   }
+//   next();
+// });
 
 const Employee = mongoose.model('Employee', employeeSchema);
 
